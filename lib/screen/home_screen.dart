591679fdd234496bd2a5834b954ea47a1d4c3a73
aldofernando29 <aldoaldo630@gmail.com>
2023-06-07 23:25:01 +0700

    homeicon

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,44 +5,78 @@ import 'package:todo_list/screen/add_screen.dart';
 import 'package:todo_list/screen/notes_card.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blueGrey,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const AddScreen(),
-              ),
-            );
-          },
-          backgroundColor: Colors.white,
-          child: const Icon(Icons.add, size: 30, color: Colors.blueGrey),
-        ),
-        appBar: AppBar(
-          title: const Text(
-            'Todo List',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+      backgroundColor: Colors.blueGrey,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddScreen(),
             ),
+          );
+        },
+        backgroundColor: Colors.white,
+        child: const Icon(Icons.add, size: 30, color: Colors.blueGrey),
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'Todo List',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
           ),
-          centerTitle: true,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
         ),
-        body: Consumer<NotesOperation>(
-          builder: (context, data, child) {
-            return ListView.builder(
-                itemCount: data.getNotes.length,
-                itemBuilder: (context, index) {
-                  return NotesCard(note: data.getNotes[index]);
-                });
-          },
-        ));
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Consumer<NotesOperation>(
+        builder: (context, data, child) {
+          return ListView.builder(
+            itemCount: data.getNotes.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AddScreen(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(15),
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.add, size: 30, color: Colors.blueGrey),
+                        const SizedBox(width: 10),
+                        Text(
+                          'Tambah Data',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+              final noteIndex = index - 1;
+              final note = data.getNotes[noteIndex];
+              return NotesCard(note: note);
+            },
+          );
+        },
+      ),
+    );
   }
 }
