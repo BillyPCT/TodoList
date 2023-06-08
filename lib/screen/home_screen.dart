6524c98b7bfada9395bbbf89071feee9fd8c56c9
aldofernando29 <aldoaldo630@gmail.com>
2023-06-08 08:20:17 +0700

    create icon view

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,40 +5,41 @@ import 'package:todo_list/screen/add_screen.dart';
 import 'package:todo_list/screen/notes_card.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.blueGrey,
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const AddScreen(),
-              ),
-            );
-          },
-          backgroundColor: Colors.white,
-          child: const Icon(Icons.add, size: 30, color: Colors.blueGrey),
-        ),
-        appBar: AppBar(
-          title: const Text(
-            'Todo List',
-            style: TextStyle(
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
+      backgroundColor: Colors.blueGrey,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddScreen(),
             ),
+          );
+        },
+        backgroundColor: Colors.white,
+        child: const Icon(Icons.add, size: 30, color: Colors.blueGrey),
+      ),
+      appBar: AppBar(
+        title: const Text(
+          'Todo List',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
           ),
-          centerTitle: true,
-          elevation: 0,
-          backgroundColor: Colors.transparent,
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              DrawerHeader(decoration: BoxDecoration(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
                 color: Colors.blue,
               ),
               child: Column(
@@ -47,46 +48,84 @@ class HomeScreen extends StatelessWidget {
                   CircleAvatar(
                     radius: 40.0,
                     backgroundColor: Colors.white,
-                    child: Icon(
+                    child: const Icon(
                       Icons.person,
                       size: 40.0,
                       color: Colors.blue,
                     ),
                   ),
-                  SizedBox(height: 16.0,),
-                  Text('TodoListt',
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+                  const SizedBox(
+                    height: 16.0,
                   ),
-                  )
+                  const Text(
+                    'TodoListt',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
                 ],
               ),
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text('home'),
-                onTap: () {
-                  // Navigasi ke halaman Home
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-                onTap: () {
-                  // Navigasi ke halaman Settings
-                },
-              )
-            ],
-          ) ),
-        body: Consumer<NotesOperation>(
-          builder: (context, data, child) {
-            return ListView.builder(
-                itemCount: data.getNotes.length,
-                itemBuilder: (context, index) {
-                  return NotesCard(note: data.getNotes[index]);
-                });
-          },
-        ));
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                // Navigasi ke halaman Home
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text('Settings'),
+              onTap: () {
+                // Navigasi ke halaman Settings
+              },
+            ),
+          ],
+        ),
+      ),
+     body: Consumer<NotesOperation>(
+        builder: (context, data, child) {
+          return ListView.builder(
+            itemCount: data.getNotes.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const AddScreen(),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.all(15),
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.add, size: 30, color: Colors.blueGrey),
+                        const SizedBox(width: 10),
+                        Text(
+                          'Tambah Data',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+              final noteIndex = index - 1;
+              final note = data.getNotes[noteIndex];
+              return NotesCard(note: note);
+            },
+          );
+        },
+      ),
+    );
   }
 }
